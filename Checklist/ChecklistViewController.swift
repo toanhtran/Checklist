584e5394946582b8fcad97a4ae1c50875b6963a5
toanhtran <toanh.t.tran@gmail.com>
2018-01-16 20:08:10 -0800

    first commit

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChecklistViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -19,7 +19,15 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+		return cell
+	}
 
 }
 
