10fd3cf331a6594b61afa953ede321e7ce40d3d5
toanhtran <toanh.t.tran@gmail.com>
2018-01-17 20:21:48 -0800

    deselect checkmark when row is selected

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -21,11 +21,38 @@ class ChecklistViewController: UITableViewController {
 	}
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 1
+		return 100
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if let cell = tableView.cellForRow(at: indexPath) {
+			if cell.accessoryType == .none {
+				cell.accessoryType = .checkmark
+			} else {
+				cell.accessoryType = .none
+			}
+			tableView.deselectRow(at: indexPath, animated: true)
+		}
 	}
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+		let label = cell.viewWithTag(1000) as! UILabel
+		
+		if indexPath.row % 5 == 0 {
+			label.text = "Walk the dog"
+		} else if indexPath.row % 5 == 1 {
+			label.text = "Brush my teeth"
+		} else if indexPath.row % 5 == 2 {
+			label.text = "Learn iOS development"
+		} else if indexPath.row % 5 == 3 {
+			label.text = "Soccer practice"
+		} else if indexPath.row % 5 == 4 {
+			label.text = "Eat ice cream"
+		} else {
+			label.text = "Sleep"
+		}
+		
 		return cell
 	}
 
