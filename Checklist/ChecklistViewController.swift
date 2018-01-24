0fbe47f259e8819d58be2bddec30f971668f1a6a
toanhtran <toanh.t.tran@gmail.com>
2018-01-23 20:27:43 -0800

    swipe to delete row

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -90,6 +90,13 @@ class ChecklistViewController: UITableViewController {
 		// Dispose of any resources that can be recreated.
 	}
 	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+		items.remove(at: indexPath.row)
+		//let indexPaths = [indexPath]
+		//tableView.deleteRows(at: indexPaths, with: .automatic)
+		tableView.reloadData()
+	}
+	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return items.count
 	}
